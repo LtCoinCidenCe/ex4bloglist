e628b13c29c2585d93869a6f1f81449662bfa3c3
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-09-08 21:56:22 +0300

    step 4.2.2

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const logger = require('./utils/logger');
+const config = require('./utils/config');
+const Blog = require('./models/blog');
+const blogsRouter = require('./controller/blogs');
+
+const app = express();
+
+logger.info('connecting to database');
+mongoose.connect(config.MONGODB_URI);
+
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter);
+
+module.exports = app;
