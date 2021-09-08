4465c44cdedc624cacd02dc09b9374a82307fc76
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-09-08 22:02:38 +0300

    exercise 4.2

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const morgan = require('morgan');
 
 const logger = require('./utils/logger');
 const config = require('./utils/config');
@@ -13,8 +14,9 @@ logger.info('connecting to database');
 mongoose.connect(config.MONGODB_URI);
 
 
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
+app.use(morgan('tiny'));
 
 app.use('/api/blogs', blogsRouter);
 
