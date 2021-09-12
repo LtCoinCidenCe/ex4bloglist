5a3a6b1ac6fef8678f1aceeca2c4c59782e212b6
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-09-12 20:34:40 +0300

    exercise 4.4 & 4.5

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -3,6 +3,41 @@ const dummy = (blogs) =>
   return 1;
 }
 
+const totalLikes = (blogs) =>
+{
+  const total = blogs.reduce(
+    (value1, value2) => { return value1 + value2.likes; },
+    0);
+  return total;
+}
+
+const favoriteBlog = (blogs) =>
+{
+  let initValue = blogs[0];
+  delete initValue.url;
+  const favorite = blogs.reduce(
+    (mostLikes, newBlog) =>
+    {
+      if (mostLikes.likes < newBlog.likes)
+      {
+        const copy = {
+          title: newBlog.title,
+          author: newBlog.author,
+          likes: newBlog.likes
+        }
+        return copy;
+      }
+      else
+      {
+        return mostLikes
+      }
+    }
+    , initValue);
+  return favorite;
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favoriteBlog
 }
