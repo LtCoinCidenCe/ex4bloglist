2a8be401b6c1c68842222f2f0887299b9184851c
Ziqi Wang <ziqi.ccc.wang@student.jyu.fi>
2021-09-30 17:40:57 +0300

    exercise 4.20

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ require('express-async-errors')
 const logger = require('./utils/logger');
 const config = require('./utils/config');
 const blogsRouter = require('./controller/blogs');
-const middleware=require('./utils/middleware');
+const middleware = require('./utils/middleware');
 const userRouter = require('./controller/users');
 const loginRouter = require('./controller/login');
 
@@ -21,6 +21,7 @@ app.use(cors());
 app.use(express.json());
 app.use(morgan('tiny'));
 
+app.use(middleware.tokenExtractor);
 app.use('/api/blogs', blogsRouter);
 app.use('/api/users', userRouter);
 app.use('/api/login', loginRouter);
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,4 +1,13 @@
 const logger = require('./logger');
+
+const tokenExtractor = (request, response, next) =>
+{
+  const authorization = request.get('authorization');
+  if (authorization && authorization.toLowerCase().startsWith('bearer '))
+    request.token = authorization.substring(7);
+  next();
+}
+
 const errorHandler = (error, request, response, next) =>
 {
   logger.error(error.message)
@@ -15,4 +24,4 @@ const errorHandler = (error, request, response, next) =>
   next(error);
 }
 
-module.exports = { errorHandler };
+module.exports = { errorHandler, tokenExtractor };
